496a48d84e7f5df75788ae9463c160cb8d6c573a
Dhaval Shah <dhavals@andrew.cmu.edu>
2011-12-06 14:52:09 -0500

    yet another test 2

--- src/edu/cmu/cs211/chess/unittested/TestedAlphaBetaFixedDepth.java
+++ src/edu/cmu/cs211/chess/unittested/TestedAlphaBetaFixedDepth.java
@@ -8,7 +8,7 @@ import edu.cmu.cs211.chess.search.AbstractSearcher;
 import java.util.List;
 
 /**
- * An implementation of Alpha Beta search.
+ * An implementation of Alpha Beta search.   blahblah
  * <p/>
  * This is the class that will be unit tested by FrontDesk.
  */
