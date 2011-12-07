7fa448e5954a8a51d5a9104ced012a8ebb5bea43
Dhaval Shah <dhavals@andrew.cmu.edu>
2011-12-07 04:55:27 -0500

    added class

---
+++ src/edu/cmu/cs211/chess/unittested/AlphaBetaQuiescentRep.java
@@ -0,0 +1,12 @@
+package edu.cmu.cs211.chess.unittested;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Dhaval
+ * Date: 12/7/11
+ * Time: 4:55 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AlphaBetaQuiescentRep
+{
+}
